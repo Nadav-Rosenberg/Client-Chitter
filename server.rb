11c5a1ab8115631613908834d483a5c23ceeba3c
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-22 18:44:16 +0100

    adding a luanchg of html page on the sever

--- server.rb
+++ server.rb
@@ -3,5 +3,9 @@ require 'sinatra/base'
 class Client < Sinatra::Base
   set :public_folder, proc { File.join(root) }
 
+  get '/' do
+    File.read('index.html')
+  end
+
   run! if app_file == $0
 end  
