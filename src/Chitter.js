36abfdfaa591ce86fd078c4d50469a2cf58d449c
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-23 10:36:42 +0100

    changing to Heroku client

--- src/Chitter.js
+++ src/Chitter.js
@@ -1,26 +1,26 @@
-// $(document).ready(function() {
-//   $.getJSON( "http://localhost:9292//api/all_peeps", function( data ) {
-//       var peep_string = '<ul>'
-//     for (var i = 0; i < data.length; i++) {
+$(document).ready(function() {
+  $.getJSON( "https://immense-beach-9973.herokuapp.com/api/all_peeps", function( data ) {
+      var peep_string = '<ul>'
+    for (var i = 0; i < data.length; i++) {
 
-//         peep_string = peep_string.concat('<li>',data[i]["text"],' by ',data[i]["author"],' at ',data[i]["time"],'</li>');
-//     };
-//     peep_string.concat('</ul>');
-//     $('#peepList').html(peep_string);
-//   });
-// });
+        peep_string = peep_string.concat('<li>',data[i]["text"],' by ',data[i]["author"],' at ',data[i]["time"],'</li>');
+    };
+    peep_string.concat('</ul>');
+    $('#peepList').html(peep_string);
+  });
+});
 
-// setInterval(function(){
-// $.getJSON( "http://localhost:9292//api/all_peeps", function( data ) {
-//     var peep_string = '<ul>'
-//   for (var i = 0; i < data.length; i++) {
+setInterval(function(){
+$.getJSON( "https://immense-beach-9973.herokuapp.com/api/all_peeps", function( data ) {
+    var peep_string = '<ul>'
+  for (var i = 0; i < data.length; i++) {
 
-//       peep_string = peep_string.concat('<li>',data[i]["text"],' by ',data[i]["author"],' at ',data[i]["time"],'</li>');
-//   };
-//   peep_string.concat('</ul>');
-//   $('#peepList').html(peep_string);
-// });
-// },60000);
+      peep_string = peep_string.concat('<li>',data[i]["text"],' by ',data[i]["author"],' at ',data[i]["time"],'</li>');
+  };
+  peep_string.concat('</ul>');
+  $('#peepList').html(peep_string);
+});
+},60000);
  
 
 
